611ad384cd0ee922ce2404ed68ccaea987c69de8
Kun Yi <63833572+aikunyi@users.noreply.github.com>
2023-11-14 23:11:35 +0800

    Add files via upload

---
+++ models/NLinear.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class Model(nn.Module):
+    """
+    Normalization-Linear
+    """
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        
+        # Use this line if you want to visualize the weights
+        # self.Linear.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+        self.channels = configs.enc_in
+        self.individual = configs.individual
+        if self.individual:
+            self.Linear = nn.ModuleList()
+            for i in range(self.channels):
+                self.Linear.append(nn.Linear(self.seq_len,self.pred_len))
+        else:
+            self.Linear = nn.Linear(self.seq_len, self.pred_len)
+
+        self.embed_size = 512
+        self.embeddings = nn.Parameter(torch.randn(1, self.embed_size))
+        self.embed_linear = nn.Linear(self.seq_len * self.embed_size, self.pred_len)
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+
+        seq_last = x[:,-1:,:].detach()
+        x = x - seq_last
+        if self.individual:
+            output = torch.zeros([x.size(0),self.pred_len,x.size(2)],dtype=x.dtype).to(x.device)
+            for i in range(self.channels):
+                output[:,:,i] = self.Linear[i](x[:,:,i])
+            x = output
+        else:
+            x = self.Linear(x.permute(0,2,1)).permute(0,2,1)
+        x = x + seq_last
+        return x
